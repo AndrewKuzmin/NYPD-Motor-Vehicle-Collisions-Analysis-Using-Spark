daf26cc6d92d9e0c531df483c394851f09e8cc09
Andrew Kuzmin <andrew.kuzmin@gmail.com>
2019-03-09 15:23:12 +0200

    Updated to last versions of libraries.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,20 +4,20 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
 
-  lazy val sparkVersion = "2.3.1"
+  lazy val sparkVersion = "2.4.0"
   lazy val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
   lazy val sparkSql  = "org.apache.spark" %% "spark-sql" % sparkVersion
   lazy val sparkHive = "org.apache.spark" %% "spark-hive" % sparkVersion
 
-  lazy val cassandra = "com.datastax.spark" %% "spark-cassandra-connector" % "2.0.7"
+  lazy val cassandra = "com.datastax.spark" % "spark-cassandra-connector_2.11" % "2.4.0"
 
 
-  lazy val typeSafeConfig = "com.typesafe" % "config" % "1.3.2"
+  lazy val typeSafeConfig = "com.typesafe" % "config" % "1.3.3"
 
-  lazy val scoptConfig = "com.github.scopt" %% "scopt" % "3.3.0"
+  lazy val scoptConfig = "com.github.scopt" %% "scopt" % "3.7.1"
 
 
-  lazy val logging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+  lazy val logging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
 
 }
 
